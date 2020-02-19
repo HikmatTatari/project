2dbcc86f957d52c8f9fff666277b0f6797fe3017
Michael <61010713+MikeWoldu@users.noreply.github.com>
2020-02-19 11:10:29 +0100

    Update MainActivity.java

--- app/src/main/java/com/lab1/project/MainActivity.java
+++ app/src/main/java/com/lab1/project/MainActivity.java
@@ -5,17 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-  
-//Konstantinos new comment
-    //second comment
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
-// Let's try this .. MW
-// THIS IS THE NEW CHANGE!!!!!!!!!!!
-// THIS IS THE NEW NEW CHANGE!!!
-//ok now 
+
